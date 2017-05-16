99a45d877822243c323706a2aa600d1529729e5f
doomc <dong9292110@163.com>
2017-05-16 23:52:48 +0800

    修改一些东西

---
+++ ZFB/Class/Main/MainController/childViews/ZFHeaderView.h
@@ -0,0 +1,13 @@
+//
+//  ZFHeaderView.h
+//  ZFB
+//
+//  Created by 熊维东 on 2017/5/16.
+//  Copyright © 2017年 com.zfb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZFHeaderView : UIView
+
+@end
---
+++ ZFB/Class/Main/MainController/childViews/ZFHeaderView.m
@@ -0,0 +1,21 @@
+//
+//  ZFHeaderView.m
+//  ZFB
+//
+//  Created by 熊维东 on 2017/5/16.
+//  Copyright © 2017年 com.zfb. All rights reserved.
+//
+
+#import "ZFHeaderView.h"
+
+@implementation ZFHeaderView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
