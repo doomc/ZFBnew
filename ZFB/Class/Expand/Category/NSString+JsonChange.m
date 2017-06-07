acdef5227f6489adaca8a91499b018867c60bce7
doomc <zhanfubao@zhanfubao-deiMac.local>
2017-06-07 19:20:28 +0800

    登录接口通了，修改登录textfiled

---
+++ ZFB/Class/Expand/Category/NSString+JsonChange.m
@@ -0,0 +1,13 @@
+//
+//  NSString+JsonChange.m
+//  ZFB
+//
+//  Created by  展富宝  on 2017/6/7.
+//  Copyright © 2017年 com.zfb. All rights reserved.
+//
+
+#import "NSString+JsonChange.h"
+
+@implementation NSString (JsonChange)
+
+@end
